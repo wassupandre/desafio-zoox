cd8e7dbd167e9b60a09602c92d37cb16e68b77c7
andre <wassupandre>
2020-09-14 19:51:38 -0300

    ajeitando erro na rota raiz

--- routes/index.route.js
+++ routes/index.route.js
@@ -6,8 +6,8 @@ import authenticate from '../helpers/authenticate.helper'
 const router = express.Router()
 
 // App routes
-router.get('/', (res,res) => {
-  return res.status(200).send({ message: 'Welcome do Desafio-zoox api.'})
+router.get('/', (req, res) => {
+  res.status(200).send('Welcome.')
 })
 router.use('/cities', authenticate,citiesRoute)
 router.use('/states', authenticate,statesRoute)
